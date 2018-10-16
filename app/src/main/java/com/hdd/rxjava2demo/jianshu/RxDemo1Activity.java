fb6e0a7cfbbd32a14601cb007c15d72b3c927a8b
hdd <2104474503@qq.com>
2018-10-16 14:27:28 +0800

    Initial commit

---
+++ app/src/main/java/com/hdd/rxjava2demo/jianshu/RxDemo1Activity.java
@@ -0,0 +1,161 @@
+package com.hdd.rxjava2demo.jianshu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.hdd.rxjava2demo.R;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+/**
+ * 上游和下游就分别对应着RxJava中的Observable和Observer，它们之间的连接就对应着subscribe()
+ *
+ */
+public class RxDemo1Activity extends AppCompatActivity {
+
+    private static final String TAG = "RxDemo1Activity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+     /*   //创建一个上游 Observable： Observable.create
+        Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                //emitter ?
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+                emitter.onComplete();
+            }
+        });
+
+        //创建一个下游 Observer
+        Observer observer = new Observer<Integer>(){
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "subscribe");
+            }
+
+            @Override
+            public void onNext(Integer value) {
+                Log.d(TAG, "" + value);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "error");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "complete");
+            }
+        };
+
+        //建立连接
+        observable.subscribe(observer);
+        //另外, subscribe()有多个重载的方法:此处用带有Observer参数的方法subscribe(Observer<? super T> observer) {}
+
+        //把以上代码连起来
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+                emitter.onComplete();
+            }
+        }).subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "subscribe");
+            }
+
+            @Override
+            public void onNext(Integer value) {
+                Log.d(TAG, "" + value);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "error");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "complete");
+            }
+        });*/
+
+     //--------------------
+        disposeTest();
+
+    }
+
+    /**
+     * 可以把它理解成两根管道之间的一个机关, 当调用它的dispose()方法时, 它就会将两根管道切断, 从而导致下游收不到事件.
+     * 注意: 调用dispose()并不会导致上游不再继续发送事件, 上游会继续发送剩余的事件.
+     */
+    private void disposeTest() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                //让上游依次发送1,2,3,complete,4
+                Log.d(TAG, "emit 1");
+                emitter.onNext(1);
+                Log.d(TAG, "emit 2");
+                emitter.onNext(2);
+                Log.d(TAG, "emit 3");
+                emitter.onNext(3);
+                Log.d(TAG, "emit complete");
+                emitter.onComplete();
+                Log.d(TAG, "emit 4");
+                emitter.onNext(4);
+            }
+        }).subscribe(new Observer<Integer>() {
+            private Disposable mDisposable;
+            private int i;
+
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "subscribe");
+                mDisposable = d;
+            }
+
+            @Override
+            public void onNext(Integer value) {
+                Log.d(TAG, "onNext: " + value);
+                i++;
+                //在下游收到第二个事件之后, 切断水管
+                if (i == 2) {
+                    Log.d(TAG, "dispose");
+                    mDisposable.dispose();
+                    Log.d(TAG, "isDisposed : " + mDisposable.isDisposed());
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "error");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "complete");
+            }
+        });
+        /**
+         * 从运行结果我们看到, 在收到onNext 2这个事件后, 切断了水管, 但是上游仍然发送了3, complete, 4这几个事件,
+         * 而且上游并没有因为发送了onComplete而停止. 同时可以看到下游的onSubscribe()方法是最先调用的
+         */
+
+    }
+}
